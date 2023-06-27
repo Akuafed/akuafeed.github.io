875c9be4c226ff70d1f0087af4b73a37f6be42d1
Akuafed <bintangbhyas@gmail.com>
2023-06-27 13:45:31 +0700

    first commit

---
+++ home.js
@@ -0,0 +1,30 @@
+
+// owl carousel script
+$('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items: 2,
+            nav: false
+        },
+        1000:{
+            items: 3,
+            nav: false
+        }
+    }
+});
+
+gsap.to("#bg", {
+    scrollTrigger : {
+      scrub : 1
+    },
+    scale : 1.5
+  });
